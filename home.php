3a6a004ef75f246fd351126a8562c39d21777500
Yipeng Song <songyip@flip3.engr.oregonstate.edu>
2017-11-16 14:30:55 -0800

    add files

---
+++ home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html> 
+    <head>
+    	 
+        <!--meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"-->
+        <title>
+        	Welcome to Formula One        </title>
+         <link rel="stylesheet">
+    </head>
+    <body>
+	<nav id="menu" class = "gradient">
+	 <ul class="horizontal">
+	 	<li><a href="http://web.engr.oregonstate.edu/~songyip/CS_340/CS_340_project/home.php">Home</a></li>
+       	<li><a href="http://web.engr.oregonstate.edu/~songyip/CS_340/CS_340_project/constructor.php">Constructors</a></li>
+		<li><a href="http://web.engr.oregonstate.edu/~songyip/CS_340/CS_340_project/driver.php">Drivers</a></li>
+		<li><a href="http://web.engr.oregonstate.edu/~songyip/CS_340/CS_340_project/circuit.php">Circuits</a></li>
+		<li><a href="http://web.engr.oregonstate.edu/~songyip/CS_340/CS_340_project/record.php">Record</a></li>
+		<li><a href="http://web.engr.oregonstate.edu/~songyip/CS_340/CS_340_project/2017result.php">2017 Result</a></li>
+	 </ul>
+	</nav>
+	<section id="content">
+		<h1>Welcome to Formula One World!</h1>
+</section>
+    </body>
+</html>
