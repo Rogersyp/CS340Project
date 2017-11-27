d539559bd465a5087a5feedc24b6810fcf799f36
Yipeng Song <songyip@flip3.engr.oregonstate.edu>
2017-11-27 14:30:07 -0800

    being able to view drivers, teams, circuits, and records

--- circuit.php
+++ circuit.php
@@ -13,7 +13,46 @@
 		<h3>Circuits</h3>
 		<section id="content">
 			<h1>Circuit page</h1>
+			<?php 
+				include 'connectvarsEECS.php'; 
+	
+				$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+				if (!$conn) {
+					die('Could not connect: ' . mysql_error());
+				}
+			// Retrieve name of table selected	
+
+				$query = "SELECT * FROM Circuits ";
+
+				$result = mysqli_query($conn, $query);
+				if (!$result) {
+					die("Query to show fields from table failed");
+				}
+			// get number of columns in table	
+				$fields_num = mysqli_num_fields($result);
+				echo "<table id='t01' border='1'><tr>";
+				
+			// printing table headers
+				for($i=0; $i<$fields_num; $i++) {	
+					$field = mysqli_fetch_field($result);	
+					echo "<td><b>$field->name</b></td>";
+				}
+				echo "</tr>\n";
+				while($row = mysqli_fetch_row($result)) {	
+					echo "<tr>";	
+					// $row is array... foreach( .. ) puts every element
+					// of $row to $cell variable	
+					foreach($row as $cell)		
+						echo "<td>$cell</td>";	
+					echo "</tr>\n";
+				}
+
+				mysqli_free_result($result);
+				mysqli_close($conn);
+
+				include('footer.php');		
+			?>
 		</section>
-		<?php include('footer.php');?>
+		
     </body>
 </html>
--- record.php
+++ record.php
@@ -11,9 +11,46 @@
     <body>
 		<?php include('navagation.php'); ?>
 		<h3>F1 Records</h3>
-		<section id="content">
-			<h1>Record page</h1>
-		</section>
+		<?php
+			include 'connectvars.php';
+			$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+			if (!$conn) {
+				die('Could not connect: ' . mysqli_error());
+			}
+			
+		// Query the database for names of all tables	
+			$result = mysqli_query($conn, "SHOW TABLES");
+			if (!$result) {
+				die("Query to show fields from table failed");
+			}
+			$num_row = mysqli_num_rows($result);
+			
+			// 	Create a pulldown menu with names of all Tables in a form
+			//   the action is to call showTable.php to diplay the contens of the table
+
+			echo "<h3>Choose one table:<h3>"; 
+			echo "<form action=\"recordinfo.php\" method=\"POST\">";
+			echo "<select name=\"myTable\" size=\"1\" Font size=\"+2\">";
+
+			// Select a database table to display
+			//	for($i=0; $i<$num_row; $i++) {
+			$tablename=mysqli_fetch_row($result);
+			echo "<option value='world_champions' >".'World Champions'."</option>";
+			echo "<option value='race_wins'>".'Race Wins'."</option>";
+			echo "<option value='pole_position'>".'Pole Position'."</option>";
+			echo "<option value='podiums'>".'Podiums'."</option>";
+			echo "<option value='fastest_lap_time'>".'Fastest Lap Time'."</option>";
+			//	}
+		
+			echo "</select>";
+			echo "<div><input type=\"submit\" value=\"Submit\"></div>";
+			echo "</form>";
+			
+			mysqli_free_result($result);
+			mysqli_close($conn);
+
+
+		?>
 		<?php include('footer.php');?>
     </body>
 </html>
