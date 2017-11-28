f3a93ecaa657445085d79a665482b40db9af5b3d
Yipeng Song <songyip@flip3.engr.oregonstate.edu>
2017-11-28 14:54:10 -0800

    add changes to record and recordinfo

--- record.php
+++ record.php
@@ -28,7 +28,7 @@
 			// 	Create a pulldown menu with names of all Tables in a form
 			//   the action is to call showTable.php to diplay the contens of the table
 
-			echo "<h3>Choose one table:<h3>"; 
+			echo "<h3>Choose one Record:<h3>"; 
 			echo "<form action=\"recordinfo.php\" method=\"POST\">";
 			echo "<select name=\"myTable\" size=\"1\" Font size=\"+2\">";
 
@@ -46,6 +46,10 @@
 			echo "<div><input type=\"submit\" value=\"Submit\"></div>";
 			echo "</form>";
 			
+
+			echo"<h3>World Champions:<h3>";
+			echo "<p> paragraph <p>";
+
 			mysqli_free_result($result);
 			mysqli_close($conn);
 
--- recordinfo.php
+++ recordinfo.php
@@ -15,6 +15,34 @@
 	if (!$conn) {
 		die('Could not connect: ' . mysql_error());
 	}
+
+	$result = mysqli_query($conn, "SHOW TABLES");
+	if (!$result) {
+		die("Query to show fields from table failed");
+	}
+	$num_row = mysqli_num_rows($result);
+	
+	// 	Create a pulldown menu with names of all Tables in a form
+	//   the action is to call showTable.php to diplay the contens of the table
+
+	echo "<h3>Choose one Record:<h3>"; 
+	echo "<form action=\"recordinfo.php\" method=\"POST\">";
+	echo "<select name=\"myTable\" size=\"1\" Font size=\"+2\">";
+
+	// Select a database table to display
+	//	for($i=0; $i<$num_row; $i++) {
+	$tablename=mysqli_fetch_row($result);
+	echo "<option value='world_champions' >".'World Champions'."</option>";
+	echo "<option value='race_wins'>".'Race Wins'."</option>";
+	echo "<option value='pole_position'>".'Pole Position'."</option>";
+	echo "<option value='podiums'>".'Podiums'."</option>";
+	echo "<option value='fastest_lap_time'>".'Fastest Lap Time'."</option>";
+	//	}
+
+	echo "</select>";
+	echo "<div><input type=\"submit\" value=\"Submit\"></div>";
+	echo "</form>";
+
 // Retrieve name of table selected	
 	$table = $_POST['myTable'];
 	$query = "SELECT * FROM $table ";
